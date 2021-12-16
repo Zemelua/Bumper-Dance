63d01604d27a08c4a0449cc463400cb71e27c327
Zemelua <red0165333@gmail.com>
2021-12-17 01:16:21 +0900

    Initialized plugin

---
+++ src/main/java/io/github/zemelua/bumper_dance/BumperDance.java
@@ -0,0 +1,17 @@
+package io.github.zemelua.bumper_dance;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BumperDance extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+
+	}
+
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+}
